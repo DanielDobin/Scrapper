16a7ca6dbe2aead84d53469a56f438392f1c92da
DanielDobin <yahides@gmail.com>
2025-03-12 00:40:44 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
-import puppeteer from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import { Solver } from '2captcha';
-import fs from 'fs';
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const { Solver } = require('2captcha');
+const fs = require('fs');
 
 puppeteer.use(StealthPlugin());
 const captchaSolver = new Solver(process.env.CAPTCHA_API_KEY);
